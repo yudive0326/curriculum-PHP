cd588fdb9ff9491ae0baff53f571b95305d99c31
yudive0326 <75841052+yudive0326@users.noreply.github.com>
2021-01-26 21:15:13 +0900

    課題提出
    2-7再提出

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,13 +1,14 @@
 <?php
 $color = ["red" => "赤", "blue" => "青", "green" => "緑"];
-$color["yello"] = "黄色";
 
-echo $color["red"];
-
-echo $color["blue"];
-
-echo $color["green"];
 var_dump($color);
 echo '<br>';
+?>
 
+<?php
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+$color["yello"] = "黄色";
+
+var_dump($color);
+echo '<br>';
 ?>
