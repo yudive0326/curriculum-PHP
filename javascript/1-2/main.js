fc0be9929dbd8bcc9d861c9f906081009aacfd02
yudive0326 <75841052+yudive0326@users.noreply.github.com>
2021-04-05 22:16:05 +0900

    課題提出
    1-2の提出

---
+++ javascript/1-2/main.js
@@ -0,0 +1,15 @@
+
+let name = "池田";
+console.log(name + "くん");
+let number = 10;
+console.log(number * 3);
+
+console.log(`${name}くんは${number}月生まれです`);
+
+
+//課題
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
