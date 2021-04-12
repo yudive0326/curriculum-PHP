f1019e839af6e20ea4e6dfa8303cff9b19f758e9
yudive0326 <75841052+yudive0326@users.noreply.github.com>
2021-04-12 20:46:58 +0900

    課題提出
    1-6の提出

---
+++ javascript/1-5/main.js
@@ -0,0 +1,22 @@
+//①関数名を適切なものに変えてください
+//②仮引数を適切なものに変えてください
+function createJuice(fruits) {
+    //③「〇〇を受け取りました。ジュースにして返します」と出力
+    console.log(fruits + 'を受け取りました。ジュースにして返します');
+
+    //④受け取った果物にジュースという文字列を結合して、呼び出し元に返す
+    console.log(fruits + 'ジュース');
+    return createJuice;
+
+    
+}
+//⑤関数の実行：「みかん」を渡して、結果を受け取れるように適当な変数（orangeJuice）を用意
+//【注文する側（関数実行）】
+let orangeJuice = createJuice('みかん');
+//⑥返ってきた変数を利用して「〇〇が届きました」と出力する
+
+console.log(createJuice　+ 'が届きました');
+
+
+
+
---
+++ javascript/1-6/main.js
@@ -0,0 +1,18 @@
+//問1
+
+let scores = [10, 15, 20, 25];
+console.log(scores);
+
+console.log(scores[0]　+ 'は偶数です');
+console.log(scores[2]　+ 'は偶数です');
+
+
+//問2：ガソリンとナンバーのプロパティを持った「carオブジェクト」を作成して、ガソリンとナンバーを出力してください。
+let car = {
+    gass: 20.5,
+    num: 1234,
+};
+console.log('ガソリンは' + car.gass + 'です');
+console.log('ナンバーは' + car.num) + 'です';
+
+
