5771736d0d57a6a2f7f649110c98f5bad008c486
yudive0326 <75841052+yudive0326@users.noreply.github.com>
2021-04-12 21:43:37 +0900

    課題提出
    1-7の提出

---
+++ javascript/1-7/main.js
@@ -0,0 +1,15 @@
+
+
+
+class Taiyaki {
+    constructor(anko, cream, cheese) {
+        this.anko = anko;
+        this.cream = cream;
+        this.cheese = cheese;
+
+    }
+}
+let taiyaki = new Taiyaki('あんこ', 'クリーム', 'チーズ');
+console.log('中身は' + taiyaki.anko + 'です');
+console.log('中身は' + taiyaki.cream + 'です');
+console.log('中身は' + taiyaki.cheese + 'です');
