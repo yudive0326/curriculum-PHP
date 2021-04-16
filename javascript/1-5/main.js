e0863ae90c7ae792cdfbe8e944d561d297fc1157
yudive0326 <75841052+yudive0326@users.noreply.github.com>
2021-04-16 20:24:58 +0900

    課題提出
    1-5の提出

--- javascript/1-5/main.js
+++ javascript/1-5/main.js
@@ -5,12 +5,12 @@ function createJuice(fruits) {
     console.log(fruits + 'を受け取りました。ジュースにして返します');
 
     //④受け取った果物にジュースという文字列を結合して、呼び出し元に返す
-    
+    return fruits + 'ジュース';
 }
 //⑤関数の実行：「みかん」を渡して、結果を受け取れるように適当な変数（orangeJuice）を用意
+let orangeJuice = createJuice('みかん');
 
-//【注文する側（関数実行）】
-createJuice('みかん');
 //⑥返ってきた変数を利用して「〇〇が届きました」と出力する
 
+console.log(orangeJuice + 'が届きました。');
 
